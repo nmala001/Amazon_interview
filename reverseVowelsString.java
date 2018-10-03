165fadff0706196cb00b7b7533e4dcdf4bc07585
Nandith <31702387+nmala001@users.noreply.github.com>
2018-10-03 08:55:19 -0400

    Reverse vowels of a string Java

---
+++ reverseVowelsString.java
@@ -0,0 +1,44 @@
+class Solution {
+    public String reverseVowels(String s) {
+        
+        ArrayList<Character> vowList = new ArrayList<Character>();
+        vowList.add('a');
+        vowList.add('e');
+        vowList.add('i');
+        vowList.add('o');
+        vowList.add('u');
+        vowList.add('A');
+        vowList.add('E');
+        vowList.add('I');
+        vowList.add('O');
+        vowList.add('U');
+        
+        char[] c = s.toLowerCase().toCharArray();
+        String res = "";
+        int i= 0;
+        int j= s.length()-1;
+        
+        
+        while(i<j){
+            if(!vowList.contains(c[i])){
+                i++;
+                continue;
+            }
+            if(!vowList.contains(c[j])){
+                j--;
+                continue;
+            }
+            
+            char fin = c[i];
+            c[i] = c[j];
+            c[j] = fin;
+            
+            i++;
+            j--;
+        }
+        
+        
+        return new String(c);
+        
+    }
+}
