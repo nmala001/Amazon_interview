a8bff726b545806ea9fdb6073393352cdf4db2d8
Nandith <31702387+nmala001@users.noreply.github.com>
2018-10-05 22:21:30 -0400

    Deleting the node of a particular value

---
+++ DeleteNodeValue.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+            ListNode helper = new ListNode(0);
+            helper.next = head;
+            ListNode n = helper;
+        
+            while(n.next != null){
+                
+                if(n.next.val == val){
+                    ListNode p =  n.next;
+                    n.next = p.next;
+                }else{ 
+                    n = n.next;  
+                }
+            }
+        return head;  
+        }         
+    }
